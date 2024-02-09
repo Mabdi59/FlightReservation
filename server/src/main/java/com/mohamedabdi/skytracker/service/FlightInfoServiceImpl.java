0947f41a8541c200373245ea4e64f5217a73a25a
Mohamed Abdi <Moha.abdi105@gmail.com>
2024-02-09 02:38:33 -0500

    New Api

--- server/src/main/java/com/mohamedabdi/skytracker/service/FlightInfoServiceImpl.java
+++ server/src/main/java/com/mohamedabdi/skytracker/service/FlightInfoServiceImpl.java
@@ -1,29 +1,38 @@
 package com.mohamedabdi.skytracker.service;
 
-import com.mohamedabdi.skytracker.model.FlightInfo;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import com.mohamedabdi.skytracker.model.FlightInfo;
 
 @Service
 public class FlightInfoServiceImpl implements FlightInfoService {
 
     private final RestTemplate restTemplate;
 
-    @Autowired
+    @Value("${flightaware.apikey}")
+    private String apiKey;
+
     public FlightInfoServiceImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
+    public FlightInfo getFlightInfoByNumber(String flightNumber) {
+        String url = "https://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?ident=" + flightNumber;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + apiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        ResponseEntity<FlightInfo> response = restTemplate.getForEntity(url, FlightInfo.class);
+        return response.getBody();
+    }
+
     @Override
     public FlightInfo getFlightInfo(String flightNumber) {
-        String apiUrl = "https://opensky-network.org/api/states/all";
-
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
-                .queryParam("icao24", flightNumber);
-
-        FlightInfo response = restTemplate.getForObject(builder.toUriString(), FlightInfo.class);
-        return response;
+        return null;
     }
 }
